9cba4c04beaedcf88e7f1689f19e345eafddb984
Yg0R2 <yg0r2@yahoo.com>
2022-01-11 15:58:19 +0100

    fix: publish SNAPSHOT to mavenLocal and release to GitHub

--- src/main/kotlin/eazy-publish-conventions.gradle.kts
+++ src/main/kotlin/eazy-publish-conventions.gradle.kts
@@ -3,36 +3,28 @@ plugins {
 }
 
 publishing {
-    val isRelease = !version.toString().endsWith("-SNAPSHOT")
-
-    repositories {
-        mavenLocal()
-
-        if (isRelease) {
-            maven {
-                name = "github"
-                url = uri("https://maven.pkg.github.com/eazyportal/${project.rootProject.name}")
-                credentials(PasswordCredentials::class)
-            }
-        }
-    }
-
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
-
-            from(project.components["kotlin"])
+    if (project.version.toString().endsWith("-SNAPSHOT")) {
+        repositories {
+            mavenLocal()
         }
 
-        if (isRelease) {
-            create<MavenPublication>("github") {
+        // publishing to mavenLocal needs to have a publication configured
+        publications {
+            create<MavenPublication>("maven") {
                 groupId = project.group.toString()
                 artifactId = project.name
                 version = project.version.toString()
 
                 from(project.components["kotlin"])
+            }
+        }
+    }
+    else { // release
+        repositories {
+            maven {
+                name = "github"
+                url = uri("https://maven.pkg.github.com/eazyportal/${project.rootProject.name}")
+                credentials(PasswordCredentials::class)
             }
         }
     }
