12d216a0881067f00c2652add8ad06529001529e
Yg0R2 <yg0r2@yahoo.com>
2022-07-25 14:01:35 +0200

    feature: implement docs-convention

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,10 @@ publishing {
 }
 
 dependencies {
-    implementation("org.cyclonedx.bom", "org.cyclonedx.bom.gradle.plugin", "+")
     implementation("org.jetbrains.kotlin", "kotlin-gradle-plugin")
+
+    implementation("org.asciidoctor.jvm.convert", "org.asciidoctor.jvm.convert.gradle.plugin", project.properties["asciidoctorPluginVersion"] as String)
+    implementation("org.asciidoctor.jvm.gems", "org.asciidoctor.jvm.gems.gradle.plugin", project.properties["asciidoctorPluginVersion"] as String)
+
+    implementation("org.cyclonedx.bom", "org.cyclonedx.bom.gradle.plugin", project.properties["cyclonedxPluginVersion"] as String)
 }
